9eadbad4712d95ffb25ab56fa60f531a8283f2ef
Adam <adamdox@gmail.com>
2020-07-09 20:32:03 +0200

    Make all request fields required

--- KolokwiumPoprawa/DTO/AddArtistRequest.cs
+++ KolokwiumPoprawa/DTO/AddArtistRequest.cs
@@ -1,14 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace KolokwiumPoprawa.DTO
 {
     public class AddArtistRequest
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Nickname { get; set; }
+        [Required]
         public DateTime DateOfBirth { get; set; }
+        [Required]
         public int IdCity { get; set; }
+        [Required]
         public int IdArtMovement { get; set; }
     }
 }
